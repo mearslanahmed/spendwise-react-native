a667b16cb03c6666db50339d527767086bca801d
mearslanahmed <mearslanahmed@gmail.com>
2025-12-30 00:15:11 +0500

    Added bottom navigation tab

--- app/(tabs)/home.tsx
+++ app/(tabs)/home.tsx
@@ -6,6 +6,7 @@ import { signOut } from 'firebase/auth'
 import { auth } from '@/config/firebase'
 import { colors } from '@/constants/theme'
 import { useAuth } from '@/contexts/authContext'
+import ScreenWrapper from '@/components/ScreenWrapper'
 
 const Home = () => {
     const {user} = useAuth();
@@ -15,12 +16,12 @@ const Home = () => {
         await signOut(auth);
     }
   return (
-    <View>
-      <Text>Home</Text>
+    <ScreenWrapper>
+      <Typo>Home</Typo>
       <Button onPress={handleLogout}>
         <Typo color={colors.black}>Logout</Typo>
       </Button>
-    </View>
+    </ScreenWrapper>
   )
 }
 
