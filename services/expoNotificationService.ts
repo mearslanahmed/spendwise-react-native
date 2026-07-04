3b86b0da087b0ccafda970977810fbafec8b9734
mearslanahmed <mearslanahmed@gmail.com>
2026-07-04 08:14:57 -0700

    fix: remove deprecated shouldShowAlert

--- services/expoNotificationService.ts
+++ services/expoNotificationService.ts
@@ -4,7 +4,6 @@ import * as Notifications from 'expo-notifications';
 
 Notifications.setNotificationHandler({
   handleNotification: async () => ({
-    shouldShowAlert: true,
     shouldPlaySound: true,
     shouldSetBadge: false,
     shouldShowBanner: true,
