c5334a434ea9a0c4aa3b855e95c0d8cc18586b8a
mearslanahmed <mearslanahmed@gmail.com>
2026-07-18 09:17:43 -0700

    fix: babel configuration for private methods

--- babel.config.js
+++ babel.config.js
@@ -3,6 +3,8 @@ module.exports = function(api) {
   return {
     presets: ['babel-preset-expo'],
     plugins: [
+      '@babel/plugin-transform-private-methods',
+      '@babel/plugin-transform-class-properties',
       ...(process.env.NODE_ENV === 'production' || process.env.BABEL_ENV === 'production' 
         ? ['transform-remove-console'] 
         : [])
